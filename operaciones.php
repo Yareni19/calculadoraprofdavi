d7764b3731be3502fb21745b6199c879e348fdcc
Yareni19 <yare.ramos16@gmail.com>
2024-06-07 00:21:44 -0600

    Operacion resta

--- operaciones.php
+++ operaciones.php
@@ -20,4 +20,15 @@
     $suma = $numero1 + $numero2;
 
     echo "La suma de $numero1 y $numero2 es: $suma\n";
+
+    //Aqui comienza la operacion de resta
+    echo "RESTA - \n";
+
+    $numero1resta = solicitarNumero("Introduce el 1er número: ");
+
+    $numero2resta = solicitarNumero("Introduce el 2do número: ");
+
+    $resta = $numero1resta - $numero2resta;
+
+    echo "La suma de $numero1resta y $numero2resta es: $resta\n";
 ?>
