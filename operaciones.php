182f68adc53458a8e7a537f5abb240aba41722a3
Yareni19 <yare.ramos16@gmail.com>
2024-06-07 00:37:34 -0600

    Operaciones y division

--- operaciones.php
+++ operaciones.php
@@ -20,4 +20,40 @@
     $suma = $numero1 + $numero2;
 
     echo "La suma de $numero1 y $numero2 es: $suma\n";
+
+    
+    //Aqui va la resta 
+    echo "La Resta es: \n";
+
+    $numero1resta = solicitarNumero("Introduce el primer número: ");
+
+    $numero2resta = solicitarNumero("Introduce el segundo número: ");
+
+    $resta = $numero1resta - $numero2resta;
+
+    echo "La suma de $numero1resta y $numero2resta es: $resta\n";
+
+
+    //Aqui va la multiplicacion 
+    echo "La Multiplicacion es:\n";
+
+    $numero1multi = solicitarNumero("Introduce el primer número: ");
+
+    $numero2multi = solicitarNumero("Introduce el segundo número: ");
+
+    $multi = $numero1multi * $numero2multi;
+
+    echo "La suma de $numero1multi y $numero2multi es: $multi\n";
+
+
+    //Aqui inicia la division
+    echo "La Division es:\n";
+
+    $numero1devi = solicitarNumero("Introduce el primer número: ");
+
+    $numero2devi = solicitarNumero("Introduce el segundo número: ");
+
+    $division = $numero1devi / $numero2devi;
+
+    echo "La division de $numero1devi y $numero2devi es: $division\n";
 ?>
